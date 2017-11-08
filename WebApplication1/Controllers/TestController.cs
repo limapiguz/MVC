6458bb4e8d104b250daee60b075beabf2a81ffb8
limapiguz <31495028+limapiguz@users.noreply.github.com>
2017-11-08 18:00:12 -0500

    first Commit
    this is my first lab in MVC

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+    }
+    public class TestController : Controller
+    {
+            public Customer GetCustomer()
+            {
+                Customer c = new Customer();
+                c.CustomerName = "Customer1";
+                c.Address = "Address1"; 
+                return c;
+        }
+
+    }
+}
