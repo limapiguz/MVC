ddeb6c3eed1d7b3bbb316b863cc8629c09e2fb76
limapiguz <31495028+limapiguz@users.noreply.github.com>
2017-11-14 18:20:33 -0500

    2d commit
    2d commit

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class Customer
-    {
-        public string CustomerName { get; set; }
-        public string Address { get; set; }
-    }
     public class TestController : Controller
     {
-            public Customer GetCustomer()
-            {
-                Customer c = new Customer();
-                c.CustomerName = "Customer1";
-                c.Address = "Address1"; 
-                return c;
+        
+        public ActionResult GetView()
+        {
+
+            Employee emp = new Employee();
+
+            emp.FirstName = "Lina";
+            emp.LastName = "Pinilla";
+            emp.Salary = 800000;
+
+            ViewData["Employee"] = emp;
+            return View("MyView");
+
         }
 
     }
